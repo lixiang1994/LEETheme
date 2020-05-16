efae7ee7abe53bc8b5fc8792f7a6cd47ef87a699
lixiang1994 <18611401994@163.com>
2020-05-16 11:25:34 +0800

    Demo 完善启动同步当前系统主题的演示

--- LEEThemeDemo/LEEThemeDemo/MainWindow.m
+++ LEEThemeDemo/LEEThemeDemo/MainWindow.m
@@ -10,6 +10,33 @@
 
 @implementation MainWindow
 
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // 根据系统样式变化 重新启用相应的主题 以达到同步的效果
+        if (@available(iOS 13.0, *)) {
+            switch (self.traitCollection.userInterfaceStyle) {
+                case UIUserInterfaceStyleLight:
+                    
+                    [LEETheme startTheme:DAY];
+                    
+                    break;
+                    
+                case UIUserInterfaceStyleDark:
+                    
+                    [LEETheme startTheme:NIGHT];
+                    
+                    break;
+                    
+                default:
+                    break;
+            }
+        }
+    }
+    return self;
+}
+
 - (void)traitCollectionDidChange:(UITraitCollection *)previousTraitCollection {
     [super traitCollectionDidChange:previousTraitCollection];
     // 根据系统样式变化 重新启用相应的主题 以达到同步的效果
